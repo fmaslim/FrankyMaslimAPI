01644fdcfb04852d125486122b4713d0e759e688
fmaslim <franky.maslim@yahoo.com>
2018-11-11 22:45:23 -0500

    Updated Qualifications.MainId FK reference to see if this translates to infinite loop

--- FrankyMaslimAPI/Models/Qualifications.cs
+++ FrankyMaslimAPI/Models/Qualifications.cs
@@ -6,11 +6,11 @@ namespace FrankyMaslimAPI.Models
     public partial class Qualifications
     {
         public int QualificationId { get; set; }
-        public int MainId { get; set; }
         public string Description { get; set; }
         public int? SortOrder { get; set; }
 
-        public Main Main { get; set; }
+		public int MainId { get; set; }
+		public Main Main { get; set; }
 
 		public override string ToString()
 		{
